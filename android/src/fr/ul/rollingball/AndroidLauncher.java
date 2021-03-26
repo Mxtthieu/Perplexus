388d5534849372b90d299fe2f126623a11516ab1
PERRIGOT Matthieu <47339340+Mxtthieu@users.noreply.github.com>
2021-03-26 04:01:56 +0100

    Add files via upload

---
+++ android/src/fr/ul/rollingball/AndroidLauncher.java
@@ -0,0 +1,17 @@
+package fr.ul.rollingball;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import fr.ul.rollingball.RollingBall;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		config.useAccelerometer = true;
+		initialize(new RollingBall(), config);
+	}
+}
